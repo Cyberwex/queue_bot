f593bbf65191e2bae5c3610f622c91e071ccf100
Farid Hasanov (IT) <farid.hasanov@azercell.com>
2024-07-16 22:07:45 +0400

    new release

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"net/http"
-	"os"
 	"strconv"
 	"strings"
 	"sync"
@@ -22,11 +19,10 @@ type QueueItem struct {
 var queues = make(map[int64][]QueueItem)
 var activeCountdowns = make(map[int64]time.Time)
 var countdownOwners = make(map[int64]int)
-var countdownCancelFuncs = make(map[int64]context.CancelFunc)
 var mu sync.Mutex
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("7430878489:AAHPTQWwgliaE7J45N7CZkoYxwN-UUhj42c")
+	bot, err := tgbotapi.NewBotAPI("YOUR_BOT_API_KEY")
 	if err != nil {
 		log.Panic(err)
 	}
@@ -38,47 +34,30 @@ func main() {
 
 	updates, err := bot.GetUpdatesChan(u)
 
-	go func() {
-		for update := range updates {
-			if update.Message == nil {
-				continue
-			}
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
 
-			if update.Message.IsCommand() {
-				switch update.Message.Command() {
-				case "start":
-					handleHelp(bot, update.Message)
-				case "join":
-					handleJoin(bot, update.Message)
-				case "stoptime":
-					handleStopTime(bot, update.Message)
-				case "queue":
-					handleQueue(bot, update.Message)
-				case "remove":
-					handleRemove(bot, update.Message)
-				case "help":
-					handleHelp(bot, update.Message)
-				default:
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Я не знаю такую команду, вы можете воспользоваться /help чтобы посмотреть список поддерживаемых команд.")
-					bot.Send(msg)
-				}
+		if update.Message.IsCommand() {
+			switch update.Message.Command() {
+			case "start":
+				handleHelp(bot, update.Message)
+			case "join":
+				handleJoin(bot, update.Message)
+			case "stoptime":
+				handleStopTime(bot, update.Message)
+			case "queue":
+				handleQueue(bot, update.Message)
+			case "remove":
+				handleRemove(bot, update.Message)
+			case "help":
+				handleHelp(bot, update.Message)
+			default:
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Я не знаю такую команду, вы можете воспользоваться /help чтобы посмотреть список поддерживаемых команд.")
+				bot.Send(msg)
 			}
 		}
-	}()
-
-	// Web server to satisfy Heroku's requirement
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, I'm alive!")
-	})
-
-	log.Printf("Listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
 	}
 }
 
@@ -109,78 +88,64 @@ func handleJoin(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 	msg.ReplyMarkup = getCommandButtons()
 	bot.Send(msg)
 
-	go handleQueue(bot, message) // Automatically show the queue after joining
-
-	// Start the countdown automatically if this user is the first in the queue
+	// Automatically start the timer if the queue was empty
 	if len(queues[chatID]) == 1 {
-		startCountdown(bot, chatID, queues[chatID][0])
+		startNextUser(bot, chatID)
 	}
 }
 
-func startCountdown(bot *tgbotapi.BotAPI, chatID int64, user QueueItem) {
-	mu.Lock()
-	if _, exists := activeCountdowns[chatID]; exists {
-		mu.Unlock()
+func startNextUser(bot *tgbotapi.BotAPI, chatID int64) {
+	if len(queues[chatID]) == 0 {
 		return
 	}
 
-	countdownOwners[chatID] = user.UserID
+	firstInQueue := queues[chatID][0]
+	queues[chatID] = queues[chatID][1:]
+	countdownOwners[chatID] = firstInQueue.UserID
 	activeCountdowns[chatID] = time.Now().Add(10 * time.Minute)
-
-	// Create a context with cancel function to manage the countdown
-	ctx, cancel := context.WithCancel(context.Background())
-	countdownCancelFuncs[chatID] = cancel
-	mu.Unlock()
 
 	startTime := time.Now()
 	endTime := startTime.Add(10 * time.Minute)
 
 	var nextInQueueMessage string
-	mu.Lock()
-	if len(queues[chatID]) > 1 {
-		nextInQueue := queues[chatID][1]
+	if len(queues[chatID]) > 0 {
+		nextInQueue := queues[chatID][0]
 		nextInQueueMessage = fmt.Sprintf("Следующий в очереди: %s", nextInQueue.Username)
 	} else {
 		nextInQueueMessage = "Очередь пуста."
 	}
-	mu.Unlock()
 
-	response := fmt.Sprintf("%s начал отсчёт времени.\nПромежуток: %s - %s\n%s", user.Username, startTime.Format("15:04:05"), endTime.Format("15:04:05"), nextInQueueMessage)
+	response := fmt.Sprintf("%s начал отсчёт времени.\nПромежуток: %s - %s\n%s", firstInQueue.Username, startTime.Format("15:04:05"), endTime.Format("15:04:05"), nextInQueueMessage)
 	msg := tgbotapi.NewMessage(chatID, response)
 	msg.ReplyMarkup = getCommandButtons()
 	bot.Send(msg)
 
-	go func() {
-		select {
-		case <-ctx.Done():
-			// Countdown was stopped early
+	time.AfterFunc(10*time.Minute, func() {
+		mu.Lock()
+		defer mu.Unlock()
+
+		if activeCountdowns[chatID].After(time.Now()) {
+			// Timer was stopped early
 			return
-		case <-time.After(10 * time.Minute):
-			mu.Lock()
-			defer mu.Unlock()
-
-			delete(activeCountdowns, chatID)
-			delete(countdownOwners, chatID)
-			delete(countdownCancelFuncs, chatID)
-
-			if len(queues[chatID]) > 0 {
-				nextInQueue := queues[chatID][0]
-				queues[chatID] = queues[chatID][1:]
-				response := fmt.Sprintf("%s, ваше время истекло. Теперь очередь %s.", user.Username, nextInQueue.Username)
-				msg := tgbotapi.NewMessage(chatID, response)
-				msg.ReplyMarkup = getCommandButtons()
-				bot.Send(msg)
-
-				// Start the countdown for the next user in the queue
-				go startCountdown(bot, chatID, nextInQueue)
-			} else {
-				response := fmt.Sprintf("%s, ваше время истекло. Очередь пуста.", user.Username)
-				msg := tgbotapi.NewMessage(chatID, response)
-				msg.ReplyMarkup = getCommandButtons()
-				bot.Send(msg)
-			}
 		}
-	}()
+
+		delete(activeCountdowns, chatID)
+		delete(countdownOwners, chatID)
+
+		if len(queues[chatID]) > 0 {
+			nextInQueue := queues[chatID][0]
+			response := fmt.Sprintf("%s, ваше время истекло. Теперь очередь %s.", firstInQueue.Username, nextInQueue.Username)
+			msg := tgbotapi.NewMessage(chatID, response)
+			msg.ReplyMarkup = getCommandButtons()
+			bot.Send(msg)
+			startNextUser(bot, chatID) // Start next user's timer
+		} else {
+			response := fmt.Sprintf("%s, ваше время истекло. Очередь пуста.", firstInQueue.Username)
+			msg := tgbotapi.NewMessage(chatID, response)
+			msg.ReplyMarkup = getCommandButtons()
+			bot.Send(msg)
+		}
+	})
 }
 
 func handleStopTime(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
@@ -197,12 +162,6 @@ func handleStopTime(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 		return
 	}
 
-	// Cancel the countdown context
-	if cancel, exists := countdownCancelFuncs[chatID]; exists {
-		cancel()
-		delete(countdownCancelFuncs, chatID)
-	}
-
 	delete(activeCountdowns, chatID)
 	delete(countdownOwners, chatID)
 
@@ -210,7 +169,6 @@ func handleStopTime(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 	if len(queues[chatID]) > 0 {
 		nextInQueue := queues[chatID][0]
 		nextInQueueMessage = fmt.Sprintf("Следующий в очереди: %s", nextInQueue.Username)
-		go startCountdown(bot, chatID, nextInQueue)
 	} else {
 		nextInQueueMessage = "Очередь пуста."
 	}
@@ -219,46 +177,9 @@ func handleStopTime(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 	msg := tgbotapi.NewMessage(chatID, response)
 	msg.ReplyMarkup = getCommandButtons()
 	bot.Send(msg)
-}
 
-func handleRemove(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
-	chatID := message.Chat.ID
-	args := strings.Split(message.CommandArguments(), " ")
-
-	if len(args) != 1 {
-		msg := tgbotapi.NewMessage(chatID, "Укажите номер позиции для удаления. Пример: /remove 2")
-		msg.ReplyMarkup = getCommandButtons()
-		bot.Send(msg)
-		return
-	}
-
-	position, err := strconv.Atoi(args[0])
-	if err != nil || position < 1 {
-		msg := tgbotapi.NewMessage(chatID, "Номер позиции должен быть положительным целым числом.")
-		msg.ReplyMarkup = getCommandButtons()
-		bot.Send(msg)
-		return
-	}
-
-	mu.Lock()
-	defer mu.Unlock()
-
-	if position > len(queues[chatID]) {
-		msg := tgbotapi.NewMessage(chatID, "Неверный номер позиции. Очередь не такая длинная.")
-		msg.ReplyMarkup = getCommandButtons()
-		bot.Send(msg)
-		return
-	}
-
-	removedUser := queues[chatID][position-1]
-	queues[chatID] = append(queues[chatID][:position-1], queues[chatID][position:]...)
-
-	response := fmt.Sprintf("Пользователь %s был удалён из очереди.", removedUser.Username)
-	msg := tgbotapi.NewMessage(chatID, response)
-	msg.ReplyMarkup = getCommandButtons()
-	bot.Send(msg)
-
-	handleQueue(bot, message)
+	// Start next user's timer
+	startNextUser(bot, chatID)
 }
 
 func handleQueue(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
@@ -284,12 +205,49 @@ func handleQueue(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 	bot.Send(msg)
 }
 
+func handleRemove(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	args := strings.Fields(message.CommandArguments())
+	if len(args) == 0 {
+		msg := tgbotapi.NewMessage(message.Chat.ID, "Пожалуйста, укажите номер в очереди для удаления. Пример: /remove 1")
+		msg.ReplyMarkup = getCommandButtons()
+		bot.Send(msg)
+		return
+	}
+
+	position, err := strconv.Atoi(args[0])
+	if err != nil || position < 1 {
+		msg := tgbotapi.NewMessage(message.Chat.ID, "Неверный номер. Пожалуйста, укажите корректный номер в очереди.")
+		msg.ReplyMarkup = getCommandButtons()
+		bot.Send(msg)
+		return
+	}
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	chatID := message.Chat.ID
+	if position > len(queues[chatID]) {
+		msg := tgbotapi.NewMessage(chatID, "Неверный номер. Пожалуйста, укажите корректный номер в очереди.")
+		msg.ReplyMarkup = getCommandButtons()
+		bot.Send(msg)
+		return
+	}
+
+	removedUser := queues[chatID][position-1]
+	queues[chatID] = append(queues[chatID][:position-1], queues[chatID][position:]...)
+
+	response := fmt.Sprintf("Пользователь %s был удален из очереди.", removedUser.Username)
+	msg := tgbotapi.NewMessage(chatID, response)
+	msg.ReplyMarkup = getCommandButtons()
+	bot.Send(msg)
+}
+
 func handleHelp(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 	response := "Доступные команды:\n" +
 		"/join - Занять очередь\n" +
 		"/stoptime - Остановить отсчёт времени (только пользователь, начавший отсчёт)\n" +
 		"/queue - Показать очередь\n" +
-		"/remove <номер> - Удалить из очереди по номеру\n" +
+		"/remove <номер> - Удалить пользователя из очереди по номеру\n" +
 		"/help - Показать доступные команды"
 
 	msg := tgbotapi.NewMessage(message.Chat.ID, response)
@@ -301,13 +259,13 @@ func getCommandButtons() tgbotapi.ReplyKeyboardMarkup {
 	return tgbotapi.NewReplyKeyboard(
 		tgbotapi.NewKeyboardButtonRow(
 			tgbotapi.NewKeyboardButton("Занять очередь /join"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
 			tgbotapi.NewKeyboardButton("Остановить отсчёт времени /stoptime"),
-		),
-		tgbotapi.NewKeyboardButtonRow(
 			tgbotapi.NewKeyboardButton("Показать очередь /queue"),
-			tgbotapi.NewKeyboardButton("Удалить из очереди /remove"),
 		),
 		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Удалить из очереди /remove"),
 			tgbotapi.NewKeyboardButton("Помощь /help"),
 		),
 	)
